2e2079e71bf5cd4be6feb4bcac7c9b3bd90bea34
lucas <lucas.apolonio.silva@hotmail.com>
2021-09-14 17:46:12 -0300

    Criação de elementos com título e tipo - OK

--- jsTestes.js
+++ jsTestes.js
@@ -1,5 +1,9 @@
-const DivContainer = document.getElementById("div-container");
+const divContainer = document.getElementById("div-container");
+const inputContainer = document.getElementById("inputContainer");
 
+inputContainer.addEventListener('keyup', clickInputTitle); //
+
+//Array auxiliar para tratar atributos de input
 let arrayAux;
 
 //Objeto Elemento
@@ -8,36 +12,119 @@ function ElementTagObject(tagHtml, attributesHtml){
     this.tagHtml = tagHtml;
     this.attributesHtml = attributesHtml;
     
-    arrayAux = this.attributesHtml.split('-');
+    if(typeof(this.attributesHtml) === "undefined"){
+        
+        console.log("É undenifed");
+
+    } else {
+        arrayAux = this.attributesHtml.split('-'); //Separa a string e joga dentro de uma array
+    }
     
     //Funcao responsável por criar o input
     this.createElement = function () {
 
         let elementHtml = document.createElement(this.tagHtml);
         
-        for(var i = 0; i < arrayAux.length; i++){
+        if(typeof(this.attributesHtml) === "undefined"){
+        
+            console.log("É undenifed");
+            
+            if(this.tagHtml != "textarea"){
+                let createTextNode = document.createTextNode(inputContainer.value)
+                elementHtml.appendChild(createTextNode);
+            }
 
-            //Faz a separação para que não haja erros no atributos do input
-            if(i  % 2 === 0){    
+            let createTextNodeNoText = document.createTextNode("");
+            elementHtml.appendChild(createTextNodeNoText);
+            
+    
+        } else {
+
+            console.log(arrayAux);
+            for(var i = 0; i < arrayAux.length; i++){
+
+                //Faz a separação para que não haja erros no atributos do input
+                //Tem que receber os atributos e valores, ex: type,text
+                if(i  % 2 === 0){    
                 
                 elementHtml.setAttribute( arrayAux[i], arrayAux[i+1] );
                 console.log(arrayAux[i], arrayAux[i+1]);
                 
-            } else {
+                } else {
             
                 console.log(arrayAux[i-1], arrayAux[i]);
+            
+                }
             
             }
 
         }
 
         console.log(elementHtml);
-        DivContainer.appendChild(elementHtml);
+        
+
+        divContainer.appendChild(elementHtml);
         
     }
 
 }
 
-const inputHtml = new ElementTagObject("input", "type-text-name-lucas-id-testeid");
+//Função que irá criar o objeto e adicionar-lo a uma Array de Inputs.
+function clickInputOption(tag, type){
+    
+    let arrayInputsOption = [];
 
-inputHtml.createElement();
+    let tagObject = new ElementTagObject(tag, type);
+    
+    tagObject.createElement();
+
+    arrayInputsOption.push(tagObject);
+}
+
+function clickInputTitle(event){
+
+    const keyPressIsEnter = event.key == "Enter";
+    let arrayInputsTitle = [];
+
+    if(keyPressIsEnter){
+
+        let tagObject = new ElementTagObject("h1");
+        tagObject.createElement();
+      
+        arrayInputsTitle.push(tagObject);
+
+        let option = updadeOptions();
+        console.log(option);
+
+        if(option == "textarea"){
+            
+            tagObject.tagHtml = "textarea"
+            tagObject.createElement();
+            arrayInputsTitle.push(tagObject);
+
+        } else {
+
+            let type = "type-";
+            let result = type + option;
+            
+            console.log(result);
+
+            clickInputOption("input", result);
+
+        }
+        
+    }   
+}
+
+
+
+
+function updadeOptions ( ){
+    
+    
+    let select = document.getElementById("optionList");
+    let option = select.options[select.selectedIndex].value;
+
+    return option;
+
+}
