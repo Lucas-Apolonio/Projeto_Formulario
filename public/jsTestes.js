759d7a61aba5398cbe75bca310f0324bbc97e13f
lucas <lucas.apolonio.silva@hotmail.com>
2021-10-21 23:11:55 -0300

    Adicionado arquivo para ver objetivos, e funcionando funcionalidade de criacoes dos botoes dinamicamente

--- public/jsTestes.js
+++ public/jsTestes.js
@@ -1,12 +1,47 @@
 const divContainer = document.getElementById("div-container");
 const inputContainer = document.getElementById("inputContainer");
 
-inputContainer.addEventListener('keyup', clickInputTitle); //
+var testeVariavel;
+
+inputContainer.addEventListener('keyup', clickInputTitle); 
 
 //Array auxiliar para tratar atributos de input
-let arrayAux;
+let arrayAux = [];
 
-//Objeto Elemento
+let arrayInputsOption = []; //Array de Objetos de Inputs. EX: input, type/text ou type/radio | Pensar melhor se é melhor separar ou deixar juntos.
+let arrayInputsTitle = []; //Array de Objetos de Tags Titles. EX: h1, textarea
+
+
+//Função de botão add mais opções Radio
+function createNewOption(){
+
+    createElementsRadio("radio");
+}
+
+//Função que irá chamar a criação do três itens.
+function callElementsRadio() {
+    createElementsRadio("radio");
+}
+
+//Está criando os três inputs, preciso validar para ele chamar novamente a função.
+
+//Função do Option, criação dos três elementos: radio, text, button
+function createElementsRadio(radio) {
+    this.radio = radio;
+
+    for(let i = 0; i < 3; i++){
+        if(i == 0){
+            clickInputOption("input", "type-radio");
+        } else if ( i == 1){
+            clickInputOption("input", "type-text");
+        } else {
+            clickInputOption("input", "type-button-value-+"); //Antigo: clickInputOption("input", "type-button-value-+-id-buttonAddRadio"+[i]);
+        }
+    }
+}
+
+
+//Objeto Elemento  
 function ElementTagObject(tagHtml, attributesHtml){
 
     this.tagHtml = tagHtml;
@@ -15,20 +50,21 @@ function ElementTagObject(tagHtml, attributesHtml){
     if(typeof(this.attributesHtml) === "undefined"){
         
         console.log("É undenifed");
+        console.log("É um elemento título: H1");
 
     } else {
-        arrayAux = this.attributesHtml.split('-'); //Separa a string e joga dentro de uma array
+        arrayAux = this.attributesHtml.split('-'); //Separa a string e joga dentro de uma array os atributos.
     }
     
-    //Funcao responsável por criar o input
+    //Funcao responsável por criar um elemento
     this.createElement = function () {
 
         let elementHtml = document.createElement(this.tagHtml);
         
+        //Valida se o elemento tem um type
         if(typeof(this.attributesHtml) === "undefined"){
-        
-            console.log("É undenifed");
             
+            //valida se o elemento é um textarea
             if(this.tagHtml != "textarea"){
                 let createTextNode = document.createTextNode(inputContainer.value)
                 elementHtml.appendChild(createTextNode);
@@ -45,14 +81,20 @@ function ElementTagObject(tagHtml, attributesHtml){
 
                 //Faz a separação para que não haja erros no atributos do input
                 //Tem que receber os atributos e valores, ex: type,text
-                if(i  % 2 === 0){    
-                
-                elementHtml.setAttribute( arrayAux[i], arrayAux[i+1] );
-                console.log(arrayAux[i], arrayAux[i+1]);
+                if(i % 2 === 0){    //Por que?  
+
+                    elementHtml.setAttribute( arrayAux[i], arrayAux[i+1] );
+
+                    //Atribuindo evento ao elemento, caso seja um botão. Talvez esse IF seja melhor na linha 41
+                    if(arrayAux[1] == "button"){
+                        elementHtml.setAttribute("onclick", "createNewOption()");
+                    }
+
+                    console.log(arrayAux[i], arrayAux[i+1]);
                 
                 } else {
             
-                console.log(arrayAux[i-1], arrayAux[i]);
+                    console.log(arrayAux[i-1], arrayAux[i]);
             
                 }
             
@@ -61,6 +103,7 @@ function ElementTagObject(tagHtml, attributesHtml){
         }
 
         console.log(elementHtml);
+
         
 
         divContainer.appendChild(elementHtml);
@@ -69,10 +112,8 @@ function ElementTagObject(tagHtml, attributesHtml){
 
 }
 
-//Função que irá criar o objeto e adicionar-lo a uma Array de Inputs.
+//Função que irá criar o objeto elemento com um tipo e adicionar-lo a uma Array de Inputs.
 function clickInputOption(tag, type){
-    
-    let arrayInputsOption = [];
 
     let tagObject = new ElementTagObject(tag, type);
     
@@ -81,46 +122,52 @@ function clickInputOption(tag, type){
     arrayInputsOption.push(tagObject);
 }
 
+
+//Primeira função a ser chamada ao evento
 function clickInputTitle(event){
 
+    //Variável que recebe o valor e validar se o evento foi igual ao valor.
     const keyPressIsEnter = event.key == "Enter";
-    let arrayInputsTitle = [];
 
     if(keyPressIsEnter){
 
+        //Cria uma elemento H1
         let tagObject = new ElementTagObject("h1");
         tagObject.createElement();
       
-        arrayInputsTitle.push(tagObject);
+        arrayInputsTitle.push(tagObject); 
 
         let option = updadeOptions();
-        console.log(option);
 
-        if(option == "textarea"){
+        if(option == "textarea"){ //Textarea é uma tag sozinha, sem necessidade de um type.
             
             tagObject.tagHtml = "textarea"
             tagObject.createElement();
-            arrayInputsTitle.push(tagObject);
+            arrayInputsOption.push(tagObject); 
 
         } else {
 
             let type = "type-";
             let result = type + option;
-            
-            console.log(result);
 
-            clickInputOption("input", result);
+            //Valida se é um radio e criará mais de uma elemento.
+            if(option == "radio"){
+
+                createElementsRadio(result);
+
+            } else {
+                
+                clickInputOption("input", result);
+
+            }
 
         }
         
     }   
 }
 
-
-
-
+//Pega o valor da lista optionList e retorna o valor
 function updadeOptions ( ){
-    
     
     let select = document.getElementById("optionList");
     let option = select.options[select.selectedIndex].value;
